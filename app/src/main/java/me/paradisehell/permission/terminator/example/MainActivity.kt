3dbc3b789ba98c62d1a1f7deeccbcfda5a889a43
ParadiseHell <tao@paradisehell.org>
2021-01-30 00:17:28 +0800

    refactor : update usage in example

--- app/src/main/java/me/paradisehell/permission/terminator/example/MainActivity.kt
+++ app/src/main/java/me/paradisehell/permission/terminator/example/MainActivity.kt
@@ -5,38 +5,112 @@ import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import me.paradisehell.permission.terminator.PermissionCallback
 import me.paradisehell.permission.terminator.PermissionTerminator
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener, PermissionCallback {
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<View>(R.id.button_audio).setOnClickListener {
-            PermissionTerminator
-                .with(this)
-                .permissions(
-                    Manifest.permission.RECORD_AUDIO,
+        findViewById<View>(R.id.button_normal_permission).setOnClickListener(this)
+        findViewById<View>(R.id.button_access_notification_policy).setOnClickListener(this)
+        findViewById<View>(R.id.button_manage_external_storage).setOnClickListener(this)
+        findViewById<View>(R.id.button_request_install_packages).setOnClickListener(this)
+        findViewById<View>(R.id.button_system_alert_window).setOnClickListener(this)
+        findViewById<View>(R.id.button_write_settings).setOnClickListener(this)
+        findViewById<View>(R.id.button_all).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.button_normal_permission -> {
+                PermissionTerminator
+                    .with(this)
+                    .permissions(
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.READ_CONTACTS,
+                        Manifest.permission.READ_CALENDAR
+                    )
+                    .request(this)
+            }
+            R.id.button_access_notification_policy -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    PermissionTerminator
+                        .with(this)
+                        .permissions(Manifest.permission.ACCESS_NOTIFICATION_POLICY)
+                        .request(this)
+                }
+            }
+            R.id.button_manage_external_storage -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    PermissionTerminator
+                        .with(this)
+                        .permissions(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
+                        .request(this)
+                }
+            }
+            R.id.button_request_install_packages -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    PermissionTerminator
+                        .with(this)
+                        .permissions(Manifest.permission.REQUEST_INSTALL_PACKAGES)
+                        .request(this)
+                }
+            }
+            R.id.button_system_alert_window -> {
+                PermissionTerminator
+                    .with(this)
+                    .permissions(Manifest.permission.SYSTEM_ALERT_WINDOW)
+                    .request(this)
+            }
+            R.id.button_write_settings -> {
+                PermissionTerminator
+                    .with(this)
+                    .permissions(Manifest.permission.WRITE_SETTINGS)
+                    .request(this)
+            }
+            R.id.button_all -> {
+                val permissionList = mutableListOf(
+                    Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.READ_CALENDAR,
                     Manifest.permission.SYSTEM_ALERT_WINDOW,
-                    Manifest.permission.REQUEST_INSTALL_PACKAGES,
-                    Manifest.permission.ACCESS_NOTIFICATION_POLICY,
-                    Manifest.permission.WRITE_SETTINGS,
-                    Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_SETTINGS
                 )
-                .request(
-                    onGranted = {
-                        Toast.makeText(this, "Granted", Toast.LENGTH_SHORT).show()
-                    },
-                    onDenied = { _, _ ->
-                        Toast.makeText(this, "Denied", Toast.LENGTH_SHORT).show()
-                    },
-                    onNeverAsked = { _, _, _ ->
-                        Toast.makeText(this, "NeverAsk", Toast.LENGTH_SHORT).show()
-                    }
-                )
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    permissionList.add(Manifest.permission.ACCESS_NOTIFICATION_POLICY)
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    permissionList.add(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    permissionList.add(Manifest.permission.REQUEST_INSTALL_PACKAGES)
+                }
+                PermissionTerminator
+                    .with(this)
+                    .permissions(
+                        Manifest.permission.RECORD_AUDIO,
+                        permissions = permissionList.toTypedArray()
+                    )
+                    .request(this)
+            }
         }
+    }
+
+    override fun onGranted(grantedPermissionList: List<String>) {
+        Toast.makeText(this, "Granted", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDenied(grantedPermissionList: List<String>, deniedPermissionList: List<String>) {
+        Toast.makeText(this, "Denied", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNeverAsked(
+        grantedPermissionList: List<String>,
+        deniedPermissionList: List<String>,
+        neverAskPermissionList: List<String>
+    ) {
+        Toast.makeText(this, "NeverAsk", Toast.LENGTH_SHORT).show()
     }
 }
